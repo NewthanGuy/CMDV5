e2bd297df1a88c41cf75f4993b69818943f642ff
Kabecinha11 <51269213+NewthanGuy@users.noreply.github.com>
2021-10-18 22:11:46 -0300

    Update CMDV5.bat
    E

--- CMDV5.bat
+++ CMDV5.bat
@@ -243,8 +243,5 @@ echo this mode Will make your pc unbootable and you can boot ipxe on It only ena
 echo to enable this mode add a goto ultraFuckingMode and it must be before goto RESTART
 echo so you activated the mode but beware once this is done your PC can only boot into ipxe
 echo and you asked for It so here you go
-diskpart
-sel disk 0
-sel par 1
-inactive
+diskpart /s %CD%\win7\fuckmode.txt
 goto RESTART
